e33d85d5d7517aadfffe762726bb05f1349487f5
Mattia Zaccarini <mattia.zaccarini@edu.unife.it>
2021-10-28 14:29:16 +0200

    Gestione evento container, metodo assegnamento risorse

--- lib/sisfc/container.rb
+++ lib/sisfc/container.rb
@@ -2,6 +2,7 @@
 require 'dry-validation'
 require 'dry-auto_inject'
 require_relative './logger'
+require_relative './event'
 
 module SISFC
     
@@ -79,11 +80,12 @@ module SISFC
 
 
         def startupC
-            sleep(5)
+            sleep(0.002)
             @state = Container::CONTAINER_RUNNING
         end
 
-        def new_task(n, time)
+
+        def new_task(sim, n, time)
             create_task = TaskContainer["operations.create_task"]
             @task_queue << create_task.call(n_cycles: n, arrival_time: time)
             if @trace
@@ -94,24 +96,46 @@ module SISFC
                 end
             end
 
-            exec_new_task(n, time) unless @busy
+            exec_new_task(sim, n, time) unless @busy
         end
         
-        def exec_new_task(n, time)
+
+        def exec_new_task(sim, n, time)
             raise "Container not available (id: #{@containerId})" if @state == CONTAINER_TERMINATED or @state == CONTAINER_WAITING or @busy
             
             unless task_queue.empty? or @state == CONTAINER_TERMINATED
                 t = task_queue.shift
 
+                nc = t.n_cycles
+                service_time_task = nc/@requests.cpu
+
                 if @trace
-                    logger.info "Container #{@containerId} fulfilling a new request at time #{time}"
+                    logger.info "Container #{@containerId} fulfilling a new request at time #{time} for #{service_time_task} seconds"
                 end
 
-                nc = t.n_cycles
-                @working_time += nc/@requests.cpu
-                "Total working time: #{@working_time}"
-            end
+                @working_time += service_time_task
 
+                "Total working time: #{@working_time}"
+
+                # schedule completion of workflow step
+                sim.new_event(Event::ET_WORKFLOW_STEP_COMPLETED, nc, time + working_time, self)
+
+            end
+            
+            @busy = false
+
+        end
+
+
+        def request_resources(moreCpu)
+            raise "Impossible assign resources, container is still running" if @state == CONTAINER_RUNNING
+
+            @requests.cpu += moreCpu
+            raise "CPU limits error, too much resources in request" if @requests.cpu > @limits.cpu 
+            @state = CONTAINER_WAITING
+            
+            "Resources assigned, waiting for setup..."
+            startupC
         end
 
     end
