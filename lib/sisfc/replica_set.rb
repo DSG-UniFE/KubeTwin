86e7ec64bc06b436a9e6d5c3911958259a5c28db
Filippo Poltronieri <filippo@MacBook-Pro-di-Filippo.local>
2021-11-12 12:01:40 +0100

    replica_set; added reference to service class

--- lib/sisfc/replica_set.rb
+++ lib/sisfc/replica_set.rb
@@ -15,11 +15,18 @@ module SISFC
     attr_reader :name, :selector, :replicas, :cluster_id
 
     # name and selector have the same value here
-    def initialize(name, cluster_id, selector, replicas)
+    # the replica set creates the pods, which are 
+    # associate to a service that provides naming,
+    # discovery, and lookup capabilities
+    def initialize(name, cluster_id, selector, replicas, service)
       @name = name
       @cluster_id = cluster_id
       @selector = selector
       @replicas = replicas
+      # do we need to keep a reference to the service
+      # class?
+      @service = service
+      # optional parameter to set the control loop?
     end
 
     # change the number of replicas
