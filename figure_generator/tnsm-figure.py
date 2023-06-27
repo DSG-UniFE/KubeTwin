9310fbde9f32f9283218ae8eacb623ed3f363358
Filippo Poltronieri <pltfpp@unife.it>
2023-06-27 17:46:43 +0200

    Use row index instead of Unix timestamp when plotting

--- figure_generator/tnsm-figure.py
+++ figure_generator/tnsm-figure.py
@@ -14,9 +14,10 @@ if len(sys.argv) < 2:
     print("fig.py <csv_file>")
     sys.exit(1)
 
-df_complete = pd.read_csv(sys.argv[1]) 
+df = pd.read_csv(sys.argv[1]) 
 #df = df_complete[df_complete['Time'] < (df_complete['Time'][0] + 135)]
-df = df_complete[df_complete['TTP'] > 0]
+#df = df_complete[df_complete['TTP'] > 0]
+#df = df_complete
 
 grouped = df.groupby('Component')['TTP']
 
@@ -34,13 +35,16 @@ plt.rc('ytick', labelsize=15)    # fontsize of the tick labels
 
 fig, ax = plt.subplots(figsize=(10,5))
 
+grouped = df.groupby('Component')
+df['Index'] = grouped.cumcount().reset_index(drop=True)
+
 for key, grp in df.groupby(['Component']):
     # calculate SMA for TTP
     grp['TTP-SMA'] = grp['TTP'].rolling(window=5).mean()
-    ax.plot(grp['Time'], grp['TTP-SMA'], marker=next(markers), color=next(colors), label=key[0])
+    ax.plot(grp['Index'], grp['TTP-SMA'], marker=next(markers), color=next(colors), label=key[0])
 
-ax.plot(time, ms1d, label='MS1-desired', alpha=0.8)
-ax.plot(time, ms2d, label='MS2-desired', alpha=0.8)
+ax.plot(df['Index'], ms1d, label='MS1-desired', alpha=0.8)
+ax.plot(df['Index'], ms2d, label='MS2-desired', alpha=0.8)
 
 plt.xlabel('Time (s)')
 plt.ylabel('Time To Process (TTP) (s)')
@@ -58,13 +62,12 @@ fig.savefig('ttp-tnsm.pdf')
 markers = itertools.cycle(('+', '.', 'o', '*')) 
 colors = itertools.cycle(('r', 'g'))
 
-df = df_complete
 
 fig, ax = plt.subplots(figsize=(10,5))
 
 
 for key, grp in df.groupby(['Component']):
-    ax.plot(grp['Time'],grp['Pods'],label=key[0], marker=next(markers), color=next(colors))
+    ax.plot(grp['Index'],grp['Pods'],label=key[0], marker=next(markers), color=next(colors))
 
 
 
@@ -79,11 +82,13 @@ fig.savefig('pods-tnsm.pdf')
 
 df = pd.read_csv(sys.argv[2])
 
+df['Index'] = df.reset_index(drop=True).index
+
 markers = itertools.cycle(('+', '.', 'o', '*')) 
 colors = itertools.cycle(('r', 'g'))
 
 fig, ax = plt.subplots(figsize=(10,5))
-ax.plot(df['Time'],df['CRequests'], color='#1f77b4')
+ax.plot(df['Index'],df['CRequests'], color='#1f77b4')
 
 plt.xlabel('Time (s)')
 plt.ylabel('Requests per Second (RPS)')
