d12501a295632357ff9e053e7405b4cbd10c49f5
Filippo Poltronieri <filippopoltronieri@MacBook-Pro-di-Filippo.local>
2024-01-25 11:00:14 +0100

    added a mps comment

--- lib/chaos_rl/train_dqn_embeddings.py
+++ lib/chaos_rl/train_dqn_embeddings.py
@@ -7,7 +7,7 @@ import time
 LOG_PATH = f"./results/dqn_{time.time()}/"
 
 env = ChaosEnv(config={})  
-
+# use device="mps" to use the GPU
 model = DQN('MlpPolicy', env, verbose=1, tensorboard_log=LOG_PATH, exploration_fraction=0.4)  
 model.learn(total_timesteps=15000)  # Model training
 model.save("models/DQN__totalSteps_" + str(25000) + str(datetime.today().strftime('%Y%m%d%H%M%S')))
