211da4b9e71d3a02b74cd06c7843205f7181f0e9
Filippo Poltronieri <filippopoltronieri@MBP-di-Filippo.lan>
2024-04-13 11:44:22 +0200

    Working script

--- lib/chaos_rl/test_deepset.py
+++ lib/chaos_rl/test_deepset.py
@@ -12,29 +12,49 @@ SEED = 2
 LOG_PATH = f"./results/ppo_deepset_test_{time.time()}/"
 NUM_ENVS = 1
 
+
 def parse_parameters():
     """
     Parse some parameters from the command line
     :return: (argparse.Namespace) the parsed arguments
     """
     parser = argparse.ArgumentParser(description="Test DeepSets")
-    parser.add_argument("--model", type=str, default=None, help="Model to use (ppo or dqn)")
-    parser.add_argument("--num_episodes", type=int, default=50, help="Number of episodes to test the model for")
+    parser.add_argument(
+        "--model", type=str, default=None, help="Model to use (ppo or dqn)"
+    )
+    parser.add_argument(
+        "--num_episodes",
+        type=int,
+        default=50,
+        help="Number of episodes to test the model for",
+    )
     args = parser.parse_args()
     return args
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     args = parse_parameters()
     num_episodes = args.num_episodes
     model_path = args.model
     if model_path is None:
-        raise ValueError("Please provide a model path to test your model!")    
+        raise ValueError("Please provide a model path to test your model!")
 
     for c in range(num_episodes):
-        env = DummyVecEnv([lambda ne=ne: ChaosEnvDeepSet(config={"env_id": ne, "log": LOG_PATH}) for ne in range(NUM_ENVS)])
-        agent = PPO_DeepSets(env, num_steps=100, n_minibatches=8, ent_coef=0.001, num_envs=NUM_ENVS, tensorboard_log=LOG_PATH)
-        #agent = DQN_DeepSets(env=env, num_steps=100, n_minibatches=8, seed=SEED, tensorboard_log=LOG_PATH)
+        env = DummyVecEnv(
+            [
+                lambda ne=ne: ChaosEnvDeepSet(config={"env_id": ne, "log": LOG_PATH})
+                for ne in range(NUM_ENVS)
+            ]
+        )
+        agent = PPO_DeepSets(
+            env,
+            num_steps=100,
+            n_minibatches=8,
+            ent_coef=0.001,
+            num_envs=NUM_ENVS,
+            tensorboard_log=LOG_PATH,
+        )
+        # agent = DQN_DeepSets(env=env, num_steps=100, n_minibatches=8, seed=SEED, tensorboard_log=LOG_PATH)
         agent.load(model_path)
         # Test the agent for 100 episodes
         obs = env.reset()
@@ -44,5 +64,4 @@ if __name__ == "__main__":
             action = agent.predict(obs, action_mask)
             obs, reward, dones, info = env.step(action)
             action_mask = np.array(env.env_method("action_masks"))
-            print("Dones: ", dones)
             done = dones[0]
