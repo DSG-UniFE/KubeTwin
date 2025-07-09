cc44074cf796a6f3384902498e96a1447b5eb171
Umbe dvlp <umbe-dvlp@L-novo.localdomain>
2025-07-09 11:26:05 +0200

    Modified /lib/kube_twin/configuration.rb to use new policies req

--- lib/kube_twin/configuration.rb
+++ lib/kube_twin/configuration.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require_relative './support/dsl_helper'
-
 require_relative './logger'
 
 require 'as-duration'
@@ -15,20 +14,22 @@ module ERV
         { distribution: :gaussian, weight: a * c, args: { mean: b, sd: c } }
       end
     end
+
     def self.RawParametersToMixtureArgsSeed(*args, seed)
       raise ArgumentError, "Arguments must be a multiple of 3!" if (args.count % 3) != 0
       args.each_slice(3).map do |(a,b,c)|
-        { distribution: :gaussian, weight: a * c, args: { mean: b, sd: c, seed: seed} }
+        { distribution: :gaussian, weight: a * c, args: { mean: b, sd: c, seed: seed } }
       end
     end
   end
+
   module GammaMixtureHelper
     def self.RawParametersToMixtureArgsSeed(*args, seed)
       raise ArgumentError, "Arguments must be a multiple of 3!" if (args.count % 3) != 0
       args.each_slice(3).map do |(a,b,c)|
-        { distribution: :gamma, weight: a, args: { scale: c, shape: b, seed: seed} }
+        { distribution: :gamma, weight: a, args: { scale: c, shape: b, seed: seed } }
       end
-end
+    end
   end
 end
 
@@ -82,7 +83,8 @@ module KUBETWIN
                  :warmup_duration,
                  :cooldown_duration,
                  :workflow_types,
-                 :seed
+                 :seed,
+                 :policies
   end
 
   class Configuration
@@ -100,27 +102,21 @@ module KUBETWIN
       @start_time + @duration
     end
 
-
-
     def validate
       # convert datetimes and integers into floats
-      @start_time      = @start_time.to_f
-      @duration        = @duration.to_f
-      @warmup_duration = @warmup_duration.to_f
+      @start_time        = @start_time.to_f
+      @duration          = @duration.to_f
+      @warmup_duration   = @warmup_duration.to_f
       @cooldown_duration = @cooldown_duration.to_f
       @cooldown_duration = 10 if @cooldown_duration.nil?
 
-      # initialize kpi_customization to empty hash if needed
       @kpi_customization ||= {}
 
-      # TODO: might want to restrict this substitution only to the :filename
-      # and :command keys
-      
-      #if @request_generation is not defined in the configuration file, use request_gen
+      # if @request_generation is not defined in the configuration file, use request_gen
       if @request_generation.nil?
         @request_generation = @request_gen
       else
-        @request_generation.each do |k,v|
+        @request_generation.each do |k, v|
           @request_generation[k] = v.gsub('<pwd>', File.expand_path(File.dirname(@filename)))
         end
       end
@@ -128,8 +124,6 @@ module KUBETWIN
       @custom_stats = [] unless defined? @custom_stats
       @seeds = {} unless defined? @seeds
 
-      # freeze everything!
-      # TODO check if everything is freezed
       IceNine.deep_freeze(@constraints)
       IceNine.deep_freeze(@customers)
       IceNine.deep_freeze(@custom_stats)
@@ -140,36 +134,25 @@ module KUBETWIN
       IceNine.deep_freeze(@evaluation)
       IceNine.deep_freeze(@kpi_customization)
       IceNine.deep_freeze(@latency_models)
-      #IceNine.deep_freeze(@request_generation)
       IceNine.deep_freeze(@seeds)
-      #IceNine.deep_freeze(@microservice_types)
-      #IceNine.deep_freeze(@start_time)
       IceNine.deep_freeze(@warmup_duration)
       IceNine.deep_freeze(@workflow_types)
     end
 
     def self.load_from_file(filename, validate: true)
-      # allow filename, string, and IO objects as input
       raise ArgumentError, "File #{filename} does not exist!" unless File.exist?(filename)
 
-      # create configuration object
       conf = Configuration.new(filename)
-
-      # take the file content and pass it to instance_eval
       conf.instance_eval(File.new(filename, 'r').read)
-
-      # validate and finalize configuration
       conf.validate if validate
-
-      # return new object
       conf
     end
-    
+
     def set_start(time)
       @start_time = time
     end
 
-    def set_rgen(filename,dist=nil)
+    def set_rgen(filename, dist = nil)
       if dist.nil?
         @request_generation[:filename] = filename
       else
